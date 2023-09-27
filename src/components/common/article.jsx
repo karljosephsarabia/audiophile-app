251d4c498ec586926445e1d303cb17c4c32f22e6
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-27 16:23:24 +0800

    Added article component

---
+++ src/components/common/article.jsx
@@ -0,0 +1,19 @@
+const Article = () => {
+    return (
+        <>
+            <article>
+                <div className="px-5 text-center">
+                    <img src="assets\shared\mobile\image-best-gear.jpg" alt="best-gear" className="rounded-lg" />
+                    <h1 className="uppercase font-bold text-[2rem] tracking-wide leading-10 py-10">Bringing you the <span className="text-[#D87D4A]">best</span> audio gear</h1>
+                    <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+                        earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
+                        rooms available for you to browse and experience a wide range of our products. Stop by our store
+                        to meet some of the fantastic people who make Audiophile the best place to buy your portable
+                        audio equipment.</p>
+                </div>
+            </article>
+        </>
+    );
+};
+
+export default Article;
