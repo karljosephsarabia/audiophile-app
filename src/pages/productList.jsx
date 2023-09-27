4b7fec8714a82dcba124cdec2bef853f95e3d654
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-27 16:25:31 +0800

    Included components on productList.jsx

--- src/pages/productList.jsx
+++ src/pages/productList.jsx
@@ -1,7 +1,19 @@
+import Header from '../components/common/header';
+import Banner from '../components/common/banner';
+import ProductListView from '../components/productListView';
+import Category from '../components/common/category';
+import Article from '../components/common/article';
+import Footer from '../components/common/footer';
+
 const ProductList = () => {
     return (
         <>
-        <p>This is Product List</p>
+            <Header />
+            <Banner />
+            <ProductListView />
+            <Category />
+            <Article />
+            <Footer />
         </>
     );
 };
