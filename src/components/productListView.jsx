4d08c525d2f0c571faae75191af49b3e1a0cbe09
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-27 16:23:00 +0800

    Added Product list view component

---
+++ src/components/productListView.jsx
@@ -0,0 +1,17 @@
+const ProductListView = () => {
+    return (
+        <>
+            <section className="pt-14 px-5">
+                <div className="text-center rounded-lg mb-28">
+                    <img src="assets\product-xx99-mark-two-headphones\mobile\image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg" />
+                    <h6 className="uppercase text-[#D87D4A] tracking-[0.6rem] font-semibold pt-8 pb-5">new product</h6>
+                    <h1 className="uppercase font-bold text-[2.2rem] leading-[2.75rem]">xx99 mark ii headphones</h1>
+                    <p className="text-[0.96rem] py-6">The new xx99 mark ii headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
+                    <button className="uppercase bg-[#D87D4A] text-white px-7 py-3 text-[0.96rem]">see product</button>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default ProductListView;
