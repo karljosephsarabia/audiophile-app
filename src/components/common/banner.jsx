06795ed976d18d1bbe930cd678caaeb6c391d10b
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-27 16:23:43 +0800

    Added banner component

---
+++ src/components/common/banner.jsx
@@ -0,0 +1,11 @@
+const Banner = () => {
+    return (
+        <>
+            <section>
+                <h1 className="uppercase font-bold text-white bg-black text-center text-2xl py-11 tracking-[0.3rem]">headphones</h1>
+            </section>
+        </>
+    );
+};
+
+export default Banner;
