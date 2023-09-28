c14ab4f28daf4b4aea92ac9455ec96186fc3c2f0
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-29 01:08:16 +0800

    minor responsive adjustment on article.jsx

--- src/components/common/article.jsx
+++ src/components/common/article.jsx
@@ -7,8 +7,8 @@ const Article = () => {
                     <img src="assets\shared\tablet\image-best-gear.jpg" alt="best-gear" className="rounded-lg hidden md:inline-block lg:hidden" />
                     <img src="assets\shared\desktop\image-best-gear.jpg" alt="best-gear" className="rounded-lg hidden lg:inline-block w-[470px]" />
                     <div className="text-center flex flex-col items-center lg:justify-center md:w-[36rem] lg:text-start lg:w-articleWidth">
-                        <h1 className="uppercase font-bold text-[2rem] tracking-wide leading-10 py-10 md:text-[2.8rem] lg:text-articleTitle lg:leading-lineHeightClamp lg:tracking-articleTitle">Bringing you the <span className="text-[#D87D4A]">best</span> audio gear</h1>
-                        <p className="lg:text-articleDesc">Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+                        <h1 className="uppercase font-bold text-[2rem] tracking-wide leading-10 py-10 md:text-[2.8rem] md:leading-[3.3rem] md:pt-14 lg:text-articleTitle lg:leading-articleTitle lg:tracking-articleTitle">Bringing you the <span className="text-[#D87D4A]">best</span> audio gear</h1>
+                        <p className="md:w-[600px] lg:w-auto lg:text-articleDesc">Located at the heart of New York City, Audiophile is the premier store for high end headphones,
                             earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
                             rooms available for you to browse and experience a wide range of our products. Stop by our store
                             to meet some of the fantastic people who make Audiophile the best place to buy your portable
