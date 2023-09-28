d864d7414ba09bb70b366c57612d944f450f4209
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-28 15:49:58 +0800

    Added responsive banner.jsx

--- src/components/common/banner.jsx
+++ src/components/common/banner.jsx
@@ -1,8 +1,8 @@
 const Banner = () => {
     return (
         <>
-            <section>
-                <h1 className="uppercase font-bold text-white bg-black text-center text-2xl py-11 tracking-[0.3rem]">headphones</h1>
+            <section className="min-h-[32vw] bg-black flex justify-center items-center md:min-h-[15.36rem] lg:min-h-[24vw]">
+                <h1 className="uppercase font-bold text-white text-[6.4vw] tracking-[0.3rem] md:text-[2.75rem] lg:text-[4.3vw]">headphones</h1>
             </section>
         </>
     );
