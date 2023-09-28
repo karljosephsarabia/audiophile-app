01592b9f302b9853cc7b332adfbc6040359dd378
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-28 15:50:30 +0800

    Added responsive productListView.jsx

--- src/components/productListView.jsx
+++ src/components/productListView.jsx
@@ -1,13 +1,40 @@
 const ProductListView = () => {
     return (
         <>
-            <section className="pt-14 px-5">
-                <div className="text-center rounded-lg mb-28">
-                    <img src="assets\product-xx99-mark-two-headphones\mobile\image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg" />
-                    <h6 className="uppercase text-[#D87D4A] tracking-[0.6rem] font-semibold pt-8 pb-5">new product</h6>
-                    <h1 className="uppercase font-bold text-[2.2rem] leading-[2.75rem]">xx99 mark ii headphones</h1>
-                    <p className="text-[0.96rem] py-6">The new xx99 mark ii headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
-                    <button className="uppercase bg-[#D87D4A] text-white px-7 py-3 text-[0.96rem]">see product</button>
+            <section className="pt-[14.95vw] px-5 container flex flex-col items-center justify-center mx-auto md:pt-[114.82px] lg:pt-clampPaddingTop">
+                <div className="flex flex-col items-center rounded-lg text-center w-[21rem] mb-28 md:w-auto lg:flex-row lg:text-start lg:gap-[10vw]">
+                    <img src="assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg md:hidden" />
+                    <img src="assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg hidden md:inline-block lg:hidden" />
+                    <img src="assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg hidden lg:inline-block w-[30.1rem]" />
+                    <div className="pt-[2rem] md:mx-[3rem] md:pt-[4.11rem] lg:pt-0 lg:mx-0 lg:w-widthClamp">
+                        <h6 className="uppercase text-[#D87D4A] tracking-[0.6rem] font-semibold pb-[1.25rem] md:tracking-[0.7rem] lg:text-clampStatus lg:tracking-[0.625vw]">new product</h6>
+                        <h1 className="uppercase font-bold text-[2.2rem] leading-[2.75rem] md:text-[2.7rem] md:leading-[3.3rem] md:tracking-widest lg:text-clampTitle lg:leading-lineHeightClamp">xx99 mark ii headphones</h1>
+                        <p className="text-[0.96rem] py-6 md:text-[1.1rem] lg:text-clampDesc lg:py-paddingXClamp">The new xx99 mark ii headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
+                        <button className="uppercase bg-[#D87D4A] text-white px-7 py-3 text-[0.96rem] md:text-[1.1rem] md:px-8 lg:text-buttonClamp lg:px-paddingXClamp">see product</button>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center rounded-lg text-center w-[21rem] mb-28 md:w-auto lg:flex-row-reverse lg:text-start lg:gap-[10vw]">
+                    <img src="assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg md:hidden" />
+                    <img src="assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg hidden md:inline-block lg:hidden" />
+                    <img src="assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg hidden lg:inline-block w-[30.1rem]" />
+                    <div className="pt-[2rem] md:mx-[3rem] md:pt-[4.11rem] lg:pt-0 lg:mx-0 lg:w-widthClamp">
+                        <h6 className="uppercase text-[#D87D4A] tracking-[0.6rem] font-semibold pb-[1.25rem] md:tracking-[0.7rem] lg:text-clampStatus lg:tracking-[0.625vw]">new product</h6>
+                        <h1 className="uppercase font-bold text-[2.2rem] leading-[2.75rem] md:text-[2.7rem] md:leading-[3.3rem] md:tracking-widest lg:text-clampTitle lg:leading-lineHeightClamp">xx99 mark ii headphones</h1>
+                        <p className="text-[0.96rem] py-6 md:text-[1.1rem] lg:text-clampDesc lg:py-paddingXClamp">The new xx99 mark ii headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
+                        <button className="uppercase bg-[#D87D4A] text-white px-7 py-3 text-[0.96rem] md:text-[1.1rem] md:px-8 lg:text-buttonClamp lg:px-paddingXClamp">see product</button>
+                    </div>
+                    
+                </div>
+                <div className="flex flex-col items-center rounded-lg text-center w-[21rem] mb-28 md:w-auto lg:flex-row lg:text-start lg:gap-[10vw]">
+                    <img src="assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg md:hidden" />
+                    <img src="assets/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg hidden md:inline-block lg:hidden" />
+                    <img src="assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg" alt="xx99-mark-two-headphones" className="rounded-lg hidden lg:inline-block w-[30.1rem]" />
+                    <div className="pt-[2rem] md:mx-[3rem] md:pt-[4.11rem] lg:pt-0 lg:mx-0 lg:w-widthClamp">
+                        <h6 className="uppercase text-[#D87D4A] tracking-[0.6rem] font-semibold pb-[1.25rem] md:tracking-[0.7rem] lg:text-clampStatus lg:tracking-[0.625vw]">new product</h6>
+                        <h1 className="uppercase font-bold text-[2.2rem] leading-[2.75rem] md:text-[2.7rem] md:leading-[3.3rem] md:tracking-widest lg:text-clampTitle lg:leading-lineHeightClamp">xx99 mark ii headphones</h1>
+                        <p className="text-[0.96rem] py-6 md:text-[1.1rem] lg:text-clampDesc lg:py-paddingXClamp">The new xx99 mark ii headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
+                        <button className="uppercase bg-[#D87D4A] text-white px-7 py-3 text-[0.96rem] md:text-[1.1rem] md:px-8 lg:text-buttonClamp lg:px-paddingXClamp">see product</button>
+                    </div>
                 </div>
             </section>
         </>
