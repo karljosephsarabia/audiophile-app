4757c9b28dc3c23821f4edc3842d66b3a474685a
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-09-28 15:49:30 +0800

    Added responsive header.jsx

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -2,10 +2,19 @@
 const Header = () => {
     return (
         <>
-            <header>
-                <nav className=" bg-black flex flex-row justify-between px-5 py-8 items-center">
-                    <img src="assets/shared/tablet/icon-hamburger.svg" alt="hambergur-menu" className=" w-4 h-4" />
-                    <img src="assets/shared/desktop/logo.svg" alt="audiophile-logo" />
+            <header className="bg-black">
+                <nav className=" bg-black flex flex-row justify-between px-5 py-8 items-center container mx-auto border-b-2">
+                    <div className="flex flex-row items-center gap-12 lg:gap-[10vw]">
+                        <img src="assets/shared/tablet/icon-hamburger.svg" alt="hambergur-menu" className=" w-4 h-4 lg:hidden" />
+                        <img src="assets/shared/desktop/logo.svg" alt="audiophile-logo" className="hidden md:inline-block" />
+                        <ul className="text-white uppercase flex-row gap-[4vw] font-bold hidden lg:flex">
+                            <li className="hover:text-orange"><a href="#">home</a></li>
+                            <li className="hover:text-orange"><a href="#">headphones</a></li>
+                            <li className="hover:text-orange"><a href="#">speakers</a></li>
+                            <li className="hover:text-orange"><a href="#">earphones</a></li>
+                        </ul>
+                    </div>
+                    <img src="assets/shared/desktop/logo.svg" alt="audiophile-logo" className="md:hidden" />
                     <img src="assets/shared/desktop/icon-cart.svg" alt="cart" />
                 </nav>
             </header>
