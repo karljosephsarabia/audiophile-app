f5547360f81440e4494c2bb4df19ed285311a725
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-02 10:26:48 +0800

    Added product detail styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,11 @@ export default {
         'articleTitle': 'clamp(38.4px, 3.75vw, 48px)',
         'articleDesc': 'clamp(14.4px, 1.407vw, 18px)',
         'footerCategory': 'clamp(12.8px, 1.25vw, 16px)',
-        'footerDesc': 'clamp(14.56px, 1.42vw, 18.18px)'
+        'footerDesc': 'clamp(14.56px, 1.42vw, 18.18px)',
+        'productDetailFeature': 'clamp(30.4px, 2.97vw, 38px)',
+        'productDetailDesc': 'clamp(13px, 1.27vw, 16.26px)',
+        'productDetailSuggestion': 'clamp(24px, 2.35vw, 30px)',
+        'productDetailSuggestionBtn': 'clamp(12px, 1.18vw, 15.10px)'
       },
       fontFamily: {
         'manrope': ['Manrope', 'sans-serif']
@@ -45,7 +49,11 @@ export default {
         'footerDesc': 'clamp(512px, 50vw, 640px)',
         'footerSocialMed': 'clamp(20px, 1.96vw, 25px)',
         'footerLine': 'clamp(112px, 10.94vw, 140px)',
-        'productdetail': 'clamp(984px, 96.1vw, 1080px)'
+        'productdetail': 'clamp(984px, 96.1vw, 1080px)',
+        'productFeature': 'clamp(550px, 53.72vw, 687.61px)',
+        'productDetailBigImage': 'clamp(562px, 54.89vw, 648.95px)',
+        'productdetailTitle': 'clamp(404px, 36.8vw, 471px)',
+        'productdetailSuggestionBtn': 'clamp(145px, 14.17vw, 181.38px)'
       },
       lineHeight: {
         'lineHeightClamp': 'clamp(40px, 3.91vw, 50px)',
@@ -54,7 +62,9 @@ export default {
       },
       gap: {
         'categoryGapClamp': 'clamp(16px, 1.57vw, 20px)',
-        'articleColGap': 'clamp(90px, 8.7vw, 112.51px)'
+        'articleColGap': 'clamp(90px, 8.7vw, 112.51px)',
+        'productDetailGap': 'clamp(128px, 12.5vw, 160px)',
+        'productDetailTitleGap': 'clamp(120px, 11.72vw, 150px)'
       },
       letterSpacing: {
         articleTitle: 'clamp(0.96px, 0.094vw, 1.2px)'
