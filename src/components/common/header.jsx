4b50ae6e10b58705cba50ddbda5f4abc7003ff4c
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-04 12:40:27 +0800

    removed unused package

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Context } from "../../context/context";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 const Header = () => {
