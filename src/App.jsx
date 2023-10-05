42cd484bd4569ec596bd8b9d141880f0ae1c36d4
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-05 11:06:46 +0800

    Refactor react router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useRoutes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage';
 import ProductList from './pages/productList';
 import ProductDetails from './pages/productDetails';
-import PageNotFound from './pages/pageNotFound';
 import { CartProvider } from './context/cartContext';
 import PageNotFound from './pages/pageNotFound';
 import { useContext } from 'react';
@@ -14,38 +13,24 @@ function App() {
 
   const { menuNav } = useContext(Context);
 
-  const routes = useRoutes(menuNav.map((menu, index) => {
-    if (index < 1) {
-      return {
-        path: menu.path,
-        element: <Homepage />
-      };
-    }
-
-    if (index > 0 && index < 4) {
-      return {
-        path: menu.path,
-        element: <ProductList />
-      };
-    }
-
-    if (index > 3) {
-      return {
-        path: menu.path,
-        element: <PageNotFound />
-      };
-    }
-  }));
-
-
   return (
     <>
 
       <CartProvider>
-        <Header />
-        {routes}
-        {/* <Route path='/ProductDetails' element={<ProductDetails />} /> */}
-        <Footer />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            {menuNav.map((menu, index) => index > 0 && index < 4 &&
+              <Route exact path={menu.path} key={index} >
+                <Route index element={<ProductList />} />
+                <Route path=':id' element={<ProductDetails />} />
+              </Route>
+            )}
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </CartProvider>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import MyProvider from './context/context.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import ContextProvider from './context/context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MyProvider>
-      <BrowserRouter>
+    <ContextProvider>
         <App />
-      </BrowserRouter>
-    </MyProvider>
+    </ContextProvider>
   </React.StrictMode>,
 );
