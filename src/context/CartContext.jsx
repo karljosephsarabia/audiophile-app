25b87dc1dcfcab2e2dd4dcaf2ffab435305a19d1
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-05 18:49:45 +0800

    set up the items on local storage and a function

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,10 +5,37 @@ export const CartProvider = (props) => {
 
     const [openModal, setOpenModal] = useState(false);
 
+    const [cardData, setCardData] = useState(() => {
+        const storedData = localStorage.getItem('items');
+        if (storedData) {
+            try {
+                return JSON.parse(storedData);
+            } catch (error) {
+                console.error('Error parsing JSON from localStorage:', error);
+            }
+        }
+        return [];
+    });
+
+    function handleAddToCartButton(item) {
+        // Update the cardData state with the new item
+        if (item.key === item.key) {
+            alert('error');
+        } else {
+            setCardData((prevData) => {
+                const newData = [...prevData, item];
+                // Store the updated CardData array in localStorage
+                localStorage.setItem('items', JSON.stringify(newData));
+                return newData;
+            });
+        }
+    };
+
     return (
         <>
             <CartContext.Provider value={{
                 openModal, setOpenModal,
+                cardData, handleAddToCartButton, handleRemoveFromCart,
             }}>
                 {props.children}
             </CartContext.Provider>
