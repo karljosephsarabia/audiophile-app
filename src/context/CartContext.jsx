aef006e1f0353a5ee23843ed27b08c14195b0615
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-05 20:57:59 +0800

    fix cartcontext functions

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,56 +1,64 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext();
 export const CartProvider = (props) => {
 
     const [openModal, setOpenModal] = useState(false);
+    const [cardData, setCardData] = useState([]);
+    const [total, setTotal] = useState(0);
 
-    const [cardData, setCardData] = useState(() => {
+    // Load cart data and total from localStorage on component mount
+    useEffect(() => {
         const storedData = localStorage.getItem('items');
         if (storedData) {
             try {
-                return JSON.parse(storedData);
+            const parsedData = JSON.parse(storedData);
+            setCardData(parsedData);
+            calculateTotal(parsedData);
             } catch (error) {
                 console.error('Error parsing JSON from localStorage:', error);
             }
         }
-        return [];
-    });
-
-    const cartCount = cardData.length;
-
+    }, []);
+  
+    // Calculate the total based on the items
+    const calculateTotal = (items) => {
+        const newTotal = items.reduce((acc, item) => acc + item.price, 0);
+        setTotal(newTotal);
+    };
+  
+    // Add an item to the cart
     function handleAddToCartButton(item) {
         const isItemInCart = cardData.some((cartItem) => cartItem.id === item.id);
         if (!isItemInCart) {
-            setCardData((prevData) => {
-                const newData = [...prevData, item];
-                localStorage.setItem('items', JSON.stringify(newData));
-                return newData;
-            });
+            const updatedCardData = [...cardData, item];
+            setCardData(updatedCardData);
+            localStorage.setItem('items', JSON.stringify(updatedCardData));
+            calculateTotal(updatedCardData);
         } else {
             alert('Item is already in the cart');
         }
     };
-
+  
+    // Remove an item from the cart
     function handleRemoveFromCart(index) {
-        // console.log('Removing item at index', index);
-        setCardData((prevData) => {
-            if (index >= 0 && index < prevData.length) {
-                const newData = [...prevData];
-                newData.splice(index, 1);
-                // console.log('New data after removal', newData); // Log the updated data
-                localStorage.setItem('items', JSON.stringify(newData));
-                return newData;
-            }
-            return prevData;
-        });
+        if (index >= 0 && index < cardData.length) {
+            const updatedCardData = [...cardData];
+            updatedCardData.splice(index, 1);
+            setCardData(updatedCardData);
+            localStorage.setItem('items', JSON.stringify(updatedCardData));
+            calculateTotal(updatedCardData);
+        }
     };
+
+    // Count the cart
+    const cartCount = cardData.length;
 
     return (
         <>
             <CartContext.Provider value={{
                 openModal, setOpenModal,
-                cardData, handleAddToCartButton, handleRemoveFromCart, cartCount,
+                cardData, handleAddToCartButton, handleRemoveFromCart, cartCount, total, 
             }}>
                 {props.children}
             </CartContext.Provider>
