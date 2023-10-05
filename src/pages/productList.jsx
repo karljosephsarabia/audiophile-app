4fbc90b8999d777c9fd25ed2423a3809bad29701
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-05 11:13:22 +0800

    added outlet for react router dom

--- src/pages/productList.jsx
+++ src/pages/productList.jsx
@@ -2,6 +2,7 @@ import Banner from '../components/common/banner';
 import ProductListView from '../components/productListView';
 import Category from '../components/common/category';
 import Article from '../components/common/article';
+import { Outlet } from 'react-router-dom';
 
 const ProductList = () => {
     return (
@@ -14,6 +15,7 @@ const ProductList = () => {
                 title="headphone"
             />
             <Article />
+            <Outlet />
         </>
     );
 };
