e6192b506fe6244a5f42c71f8e103f5f862f8633
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-05 11:14:44 +0800

    added product detail function on main.jsx

--- src/components/productDetailsComponents/main.jsx
+++ src/components/productDetailsComponents/main.jsx
@@ -1,15 +1,31 @@
+import { useContext, useEffect, useState } from "react";
 import ProductGallery from "./main components/productGallery";
 import ProductInfo from "./main components/productInfo";
 import ProductSuggestion from "./main components/productSuggestion";
+import { Context } from "../../context/context";
 
 const Main = () => {
+
+    const { data, selectedItem } = useContext(Context);
+    const [itemView, setItemView] = useState([]);
+
+    useEffect(() => {
+        const filterData = () => data.forEach(item => {
+            if (item.slug === selectedItem) {
+                setItemView(item);
+            }
+        });
+
+        filterData();
+    }, []);
+
     return (
         <>
-            <section className="px-5 mx-auto">
-                <ProductInfo />
-                <ProductGallery />
-                <ProductSuggestion />
-            </section>
+            {itemView?.name && <section className="px-5 mx-auto">
+                <ProductInfo item={itemView} status={itemView.new} />
+                <ProductGallery item={itemView} />
+                <ProductSuggestion item={itemView} />
+            </section>}
         </>
     );
 };
