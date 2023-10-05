efe0f2d20dff1b34fa2c7b01dd3609bbe775f7cf
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-05 14:18:42 +0800

    added back button function

--- src/components/productDetailsComponents/back-button.jsx
+++ src/components/productDetailsComponents/back-button.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const BackButton = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <section className="container px-5 mx-auto">
                 <div className="text-gray-400 font-semibold mt-5 text-start lg:mt-productDetailTitle lg:text-clampStatus">
-                    <button >Go Back</button>
+                    <button onClick={() => navigate(-1)}>Go Back</button>
                 </div>
             </section >
         </>
