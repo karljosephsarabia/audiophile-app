7c6b3a6ae958a5eee0dd3a0a1d5523a90656fea4
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-10-06 14:38:58 +0800

    Added function for no item found using useparams

--- src/components/productDetailsComponents/main.jsx
+++ src/components/productDetailsComponents/main.jsx
@@ -11,13 +11,20 @@ const Main = () => {
     const { id } = useParams();
     const itemView = data?.find(item => item.slug === id);
 
+
     return (
         <>
-            {itemView?.name && <section className="px-5 mx-auto">
-                <ProductInfo item={itemView} status={itemView.new} />
-                <ProductGallery item={itemView} />
-                <ProductSuggestion item={itemView} data={data}/>
-            </section>}
+
+            <section className="px-5 mx-auto">
+                {itemView ?
+                    <div>
+                        <ProductInfo item={itemView} status={itemView.new} />
+                        <ProductGallery item={itemView} />
+                        <ProductSuggestion item={itemView} data={data} />
+                    </div>
+                    :
+                    <h1 className="mx-auto w-[335.2px] md:w-[728px] lg:w-productdetail lg:text-clampStatus font-semibold text-gray-400 uppercase mt-10 mb-10">Sorry, no product found in this category</h1>}
+            </section>
         </>
     );
 };
